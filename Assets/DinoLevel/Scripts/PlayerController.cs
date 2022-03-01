391497f6738a33cb5fa41c6043ad86a7a75ea123
Michael Hancianu <hancianu@student.tugraz.at>
2022-03-01 16:54:49 +0100

    add player and waterfall animation, player react to animation

--- Assets/DinoLevel/Scripts/PlayerController.cs
+++ Assets/DinoLevel/Scripts/PlayerController.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     [Header("Player Movement")]
-    [SerializeField] private float speed = 1;
-    [SerializeField] private float jumpForce = 5;
-   // [SerializeField] private int extraJumps = 2;
+    [SerializeField] private float speed = 9;
+    [SerializeField] private float jumpForce = 6;
+    [SerializeField] private int extraJumps = 1;
 
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;  //game object for the middlepoint 
-    [SerializeField] private float checkRadius;
+    [SerializeField] private float checkRadius = (float)0.02;
     [SerializeField] private LayerMask whatIsGround; //mask to detect if in radius of groundCheck 
 
     [Header("Count")]
     [SerializeField] private UnityEngine.UI.Text countText;
 
+    private float _movement = 0;
     private bool _facingRight = true;
     private int _jumpCount = 0;
+    private bool _isJumping = false;
    
 
     private bool IsGrounded => Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
@@ -29,23 +33,16 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>(); 
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            _rigidbody.velocity = Vector2.up * jumpForce;
-            _jumpCount++;
-        }
+        
 
-
-        //if (IsGrounded)
-        //{
-        //    _jumpCount = 0;
-        //}
+        
 
         //if (Input.GetButtonDown("Jump") && _jumpCount < extraJumps)
         //{
@@ -57,15 +54,33 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (IsGrounded)
+        {
+            _jumpCount = 0;
+           _isJumping = false;
+        }
+        
+        if (Input.GetButtonDown("Jump") && _jumpCount < extraJumps)
+        {
+            _rigidbody.velocity = Vector2.up * jumpForce;
+            _jumpCount++;
+            _isJumping = true;
+            _animator.SetTrigger("jump1");
+            //_animator.SetBool("jump_bool", _isJumping);
+        }
 
-        var movement = Input.GetAxis("Horizontal"); 
-        Debug.Log(movement); 
+        _movement = Input.GetAxis("Horizontal"); 
+        //Debug.Log(_movement); 
+        if(_movement == 0 && _isJumping == false)
+            _animator.SetTrigger("idle");
+        else if(_movement != 0 && _isJumping == false)
+            _animator.SetTrigger("run");
+        
+        _rigidbody.velocity = new Vector2(_movement * speed, _rigidbody.velocity.y);
 
-        _rigidbody.velocity = new Vector2(movement * speed, _rigidbody.velocity.y);
-
-        if (!_facingRight && movement > 0)
+        if (!_facingRight && _movement > 0)
             Flip();
-        else if (_facingRight && movement < 0)
+        else if (_facingRight && _movement < 0)
             Flip();
 
         Camera.main.transform.position =
@@ -82,17 +97,14 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (!collision.CompareTag("Coin"))
-    //        return;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Killing"))
+            _animator.SetTrigger("dead");
 
-    //    collision.gameObject.SetActive(false); //just deactivate
-    //    //Destroy(collision.gameObject);
-    //    coinCount++;
-
-    //    countText.text = $"Coins: {coinCount}";
-    //}
+        //collision.gameObject.SetActive(false); //just deactivate
+        //Destroy(collision.gameObject);
+    }
 
 
 }
